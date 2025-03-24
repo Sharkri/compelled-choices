70f88c28836d9aa469dd4f7b555747622564dc94
Sharkri <sharkdude955@gmail.com>
2025-03-24 15:32:08 -0500

    add  lavender

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -3,20 +3,36 @@ import "/src/css/Plant.css";
 import rose from "/src/assets/rose.png";
 import rose2 from "/src/assets/rose-2.png";
 import rose3 from "/src/assets/rose-3.png";
+import lavender3 from "/src/assets/lavender.png";
+import lavender2 from "/src/assets/lavender-dead.png";
+import lavender from "/src/assets/lavender-grown.png";
 
 const Plant = ({ compassion }) => {
   const [plantPositions, setPlantPositions] = useState([]);
 
   useEffect(() => {
     const positions = Array.from({ length: 10 }, () => ({
-      x: Math.random() * 100, // Random X position (0% to 100%)
-      y: Math.random() * 20 + 5, // Random Y position (5% to 25%)
+      x: 5 + Math.random() * 90, // Random X position (0% to 100%)
+      y: Math.random() * 20 + 7, // Random Y position (5% to 25%)
+      type: Math.random() > 0.5 ? "rose" : "lavender", // Randomly choose plant type
     }));
     setPlantPositions(positions);
   }, []);
 
+  const getPlantImage = (type, compassion) => {
+    if (compassion < 30) {
+      return type === "rose" ? rose3 : lavender2;
+    } else if (compassion < 70) {
+      return type === "rose" ? rose2 : lavender3;
+    } else {
+      return type === "rose" ? rose : lavender;
+    }
+  };
+
   const plants = plantPositions.map((pos, index) => {
-    const { x, y } = pos;
+    const { x, y, type } = pos;
+    const plantImage = getPlantImage(type, compassion);
+
     return (
       <div
         className="plant-container"
@@ -27,11 +43,7 @@ const Plant = ({ compassion }) => {
         }}
       >
         <div className="container">
-          <img
-            src={compassion < 30 ? rose3 : compassion < 70 ? rose2 : rose}
-            alt="flower"
-            className="plant"
-          />
+          <img src={plantImage} alt={type} className="plant" />
           {compassion > 30 && <div className="growth-particles"></div>}
         </div>
       </div>
